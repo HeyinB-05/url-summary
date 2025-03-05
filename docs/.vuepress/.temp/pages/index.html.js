425ab17736abfce85cddf396cdf3d262588ff672
HeYinB <3164249018@qq.com>
2025-03-05 13:52:45 +0800

    打包

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "E:/我的项目/UrlSummary/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"首页\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"首页\",\"heroImage\":\"/logo.png\",\"actions\":[{\"text\":\"开始了解→\",\"link\":\"/plugin/\",\"type\":\"primary\"}],\"footer\":\"MIT Licensed | Copyright © 2025-HeyinB_\"},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"首页\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"首页\",\"heroImage\":\"/logo.png\",\"actions\":[{\"text\":\"开始了解→\",\"link\":\"/plugin/\",\"type\":\"primary\"}],\"footer\":\"MIT Licensed | Copyright © 2025-HeyinB_\"},\"headers\":[],\"git\":{\"updatedTime\":1741153307000,\"contributors\":[{\"name\":\"HeYinB\",\"username\":\"HeYinB\",\"email\":\"3164249018@qq.com\",\"commits\":1,\"url\":\"https://github.com/HeYinB\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/plugin/index.html.js
+++ docs/.vuepress/.temp/pages/plugin/index.html.js
@@ -1,16 +1,3 @@
 import comp from "E:/我的项目/UrlSummary/docs/.vuepress/.temp/pages/plugin/index.html.vue"
-const data = JSON.parse("{\"path\":\"/plugin/\",\"title\":\"推荐库\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"有趣\",\"slug\":\"有趣\",\"link\":\"#有趣\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"plugin/README.md\"}")
+const data = JSON.parse("{\"path\":\"/plugin/\",\"title\":\"推荐库\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"有趣\",\"slug\":\"有趣\",\"link\":\"#有趣\",\"children\":[]}],\"git\":{\"updatedTime\":1741153307000,\"contributors\":[{\"name\":\"HeYinB\",\"username\":\"HeYinB\",\"email\":\"3164249018@qq.com\",\"commits\":1,\"url\":\"https://github.com/HeYinB\"}]},\"filePathRelative\":\"plugin/README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
